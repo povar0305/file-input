719a6fed86b9c5814b2d91eb4a03fc6ce79dcbb5
Povarnitsina Sonya <73982948+povar0305@users.noreply.github.com>
2024-04-01 12:38:50 +0300

    start

---
+++ src/components/file-input.vue
@@ -0,0 +1,21 @@
+<script lang="ts" setup>
+import VButton from "@/components/v-button.vue";
+import { ref } from "vue";
+
+const inputRef = ref<HTMLInputElement>();
+</script>
+
+<template>
+  <label
+    >File
+    <v-button @click="inputRef.click()"> text</v-button>
+    <input ref="inputRef" type="file" />
+  </label>
+</template>
+
+<style scoped>
+input {
+  position: absolute;
+  left: -9999px;
+}
+</style>
---
+++ src/components/v-button.vue
@@ -0,0 +1,10 @@
+<script lang="ts" setup></script>
+
+<template>
+  <button>
+    <slot />
+    button
+  </button>
+</template>
+
+<style scoped></style>
